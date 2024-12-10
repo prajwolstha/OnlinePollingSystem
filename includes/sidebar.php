f78d4670049b5e3078ef574bca7f095480d926bc
prajwolstha <prazolstha12345@gmail.com>
2024-12-10 17:20:01 +0545

    Slidebar code Updated

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,33 +1,33 @@
-<div class="sidebar" style="width: 250px; background-color: #f1f1f1; height: 100vh; position: fixed; top: 0; left: 0; box-shadow: 2px 0 5px rgba(0,0,0,0.1);">
+<div class="sidebar" style="width: 250px; background-color: #0B1042; height: 100vh; position: fixed; top: 0; left: 0; box-shadow: 2px 0 5px rgba(0,0,0,0.1);">
     <div class="sidebar-header" style="padding: 20px; text-align: center;">
-        <img src="pear_logo.png" alt="Prayojan" style="width: 50px; height: auto; margin-bottom: 10px;">
-        <h4 style="margin: 0; font-family: Arial, sans-serif; font-weight: bold;">PRAYOJAN</h4>
+        <img src="../icons/mainlogo.png" alt="Prayojan" style="width: 80px; height: auto; margin-bottom: 10px;">
+        <h4 style="margin: 0; font-family: Arial, sans-serif; font-weight: bold; color: #ffffff;">PRAYOJAN</h4>
     </div>
     <ul class="sidebar-menu" style="list-style: none; padding: 0; margin: 0;">
         <!-- Home -->
         <li style="padding: 10px 20px; cursor: pointer;">
-            <a href="index.php" style="text-decoration: none; color: black; display: flex; align-items: center;">
-                <img src="Home.png" alt="Home" style="width: 20px; height: 20px; margin-right: 10px;">
+            <a href="index.php" style="text-decoration: none; color: #ffffff; display: flex; align-items: center;">
+                <img src="../icons/home.png" alt="Home" style="width: 20px; height: 20px; margin-right: 10px;">
                 <span style="font-size: 16px; font-weight: bold;">HOME</span>
             </a>
         </li>
         
         <!-- Manage Polls -->
         <li style="padding: 10px 20px;">
-            <a href="manage_polls.php" style="text-decoration: none; color: black; display: flex; align-items: center;">
-                <img src="managePolls.png" alt="Manage Polls" style="width: 20px; height: 20px; margin-right: 10px;">
+            <a href="manage_polls.php" style="text-decoration: none; color: #ffffff; display: flex; align-items: center;">
+                <img src="../icons/managePolls.png" alt="Manage Polls" style="width: 20px; height: 20px; margin-right: 10px;">
                 <span style="font-size: 16px; font-weight: bold;">MANAGE POLLS</span>
             </a>
             <ul style="list-style: none; padding-left: 30px; margin-top: 5px;">
                 <li style="margin: 5px 0;">
-                    <a href="new_poll.php" style="text-decoration: none; color: black;">
-                        <img src="newpoll.png" alt="New Poll" style="width: 15px; height: 15px; margin-right: 5px;"> 
+                    <a href="new_poll.php" style="text-decoration: none; color: #ffffff;">
+                        <img src="../icons/add.png" alt="New Poll" style="width: 15px; height: 15px; margin-right: 5px;"> 
                         NEW POLL
                     </a>
                 </li>
                 <li style="margin: 5px 0;">
-                    <a href="edit_poll.php" style="text-decoration: none; color: black;">
-                        <img src="editpoll.png" alt="Edit Poll" style="width: 15px; height: 15px; margin-right: 5px;"> 
+                    <a href="edit_poll.php" style="text-decoration: none; color: #ffffff;">
+                        <img src="../icons/edit.png" alt="Edit Poll" style="width: 15px; height: 15px; margin-right: 5px;"> 
                         EDIT POLL
                     </a>
                 </li>
@@ -36,16 +36,16 @@
         
         <!-- Manage Users -->
         <li style="padding: 10px 20px;">
-            <a href="manage_users.php" style="text-decoration: none; color: black; display: flex; align-items: center;">
-                <img src="manageUser.png" alt="Manage Users" style="width: 20px; height: 20px; margin-right: 10px;">
+            <a href="manage_users.php" style="text-decoration: none; color: #ffffff; display: flex; align-items: center;">
+                <img src="../icons/manageUser.png" alt="Manage Users" style="width: 20px; height: 20px; margin-right: 10px;">
                 <span style="font-size: 16px; font-weight: bold;">MANAGE USERS</span>
             </a>
         </li>
         
         <!-- Notifications -->
         <li style="padding: 10px 20px;">
-            <a href="notifications.php" style="text-decoration: none; color: black; display: flex; align-items: center;">
-                <img src="Notification.png" alt="Notifications" style="width: 20px; height: 20px; margin-right: 10px;">
+            <a href="notifications.php" style="text-decoration: none; color: #ffffff; display: flex; align-items: center;">
+                <img src="../icons/Notification.png" alt="Notifications" style="width: 20px; height: 20px; margin-right: 10px;">
                 <span style="font-size: 16px; font-weight: bold;">NOTIFICATIONS</span>
             </a>
         </li>
